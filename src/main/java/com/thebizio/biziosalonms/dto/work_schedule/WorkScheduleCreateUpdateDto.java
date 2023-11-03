c25f9790c410e748a40ae0ff72da4bba664f0c02
Amit <amit.k@digitalairtech.com>
2023-11-03 16:37:46 +0530

    validation added

--- src/main/java/com/thebizio/biziosalonms/dto/work_schedule/WorkScheduleCreateUpdateDto.java
+++ src/main/java/com/thebizio/biziosalonms/dto/work_schedule/WorkScheduleCreateUpdateDto.java
@@ -3,11 +3,20 @@ package com.thebizio.biziosalonms.dto.work_schedule;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class WorkScheduleCreateUpdateDto {
+    @NotNull @NotBlank
     private String name;
+
+    @NotNull
+    @NotEmpty
+    @Valid
     List<WsItem> items;
 }
--- src/main/java/com/thebizio/biziosalonms/dto/work_schedule/WsItem.java
+++ src/main/java/com/thebizio/biziosalonms/dto/work_schedule/WsItem.java
@@ -5,15 +5,22 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class WsItem {
+    @NotNull
     private WorkScheduleDayEnum day;
+
+    @NotNull
     private LocalTime startTime;
+
     private LocalTime breakStartTime;
     private LocalTime breakEndTime;
+
+    @NotNull
     private LocalTime endTime;
 }
